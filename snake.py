adb30d67ae827beb5fdca892b2251b614bb90012
Florian Bruderer <florian.bruderer@gmail.com>
2021-11-29 15:27:39 +0100

    Added food and game_over screen

--- snake.py
+++ snake.py
@@ -10,18 +10,42 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for pos in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(pos)
-            self.segments.append(new_segment)
+            self.add_tale(pos)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def add_tale(self, pos):
+        new_segment = Turtle("square")
+        new_segment.speed("fastest")
+        new_segment.color("blue")
+        new_segment.penup()
+        new_segment.goto(pos)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_tale(self.segments[-1].position())
+
+    def up(self):
+        if self.head.heading() == 0 or self.head.heading() == 180:
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() == 0 or self.head.heading() == 180:
+            self.head.setheading(270)
+
+    def left(self):
+        if self.head.heading() == 90 or self.head.heading() == 270:
+            self.head.setheading(-180)
+
+    def right(self):
+        if self.head.heading() == 90 or self.head.heading() == 270:
+            self.head.setheading(0)
